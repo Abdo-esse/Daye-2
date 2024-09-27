2b4e93316dd4844301fd1e816fb286d210142a0f
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-09-27 15:49:03 +0200

    J'ai termine le 4eme challenge du tableau

---
+++ Exercices sur les Tableaux en C/Challenge 4 -Trouver le Maximum.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,n,x,y;
+    printf("Veuillez enrer le nombre d'element du tableau : ");
+    scanf("%d",&n);
+    int t[n];
+
+
+       for (i=0;i<n;i++){
+        printf("Veuillez saisir ces elements %d:  ",i+1);
+        scanf("%d",&t[i]);
+       }
+ x=t[0];
+        for (i=0;i<n;i++)
+          if (x<t[i]){
+            x=t[i];
+
+          }
+          printf("le plus grand element dans le tableau est : %d",x);
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
