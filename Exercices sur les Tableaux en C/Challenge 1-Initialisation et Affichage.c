c9ac9de4bb7c3fbc13544786af41cad8e0d02956
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-09-27 13:31:51 +0200

    Challenge 1 du tableau

---
+++ Exercices sur les Tableaux en C/Challenge 1-Initialisation et Affichage.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+
+    int tableau[5]={1,2,3,4,5};
+
+    for (i=0;i<5;i++)
+        printf("%d ",tableau[i]);
+
+    return 0;
+}
